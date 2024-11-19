7d3e924ea17cdaaddbc71da58086a9b840a692d0
HelleFH <142264401+HelleFH@users.noreply.github.com>
2024-11-19 05:53:55 +0100

    Update projects.js

--- src/projects.js
+++ src/projects.js
@@ -34,7 +34,7 @@ const projects = [
         name: "MERN Listings",
         technologies: "MongoDB, Express, React, Node.js",
         images: ["/images/listing-screen.png"],
-        projectLink: "https://react-listings-frontend.onrender.com/",
+        projectLink: "https://react-listings-1.onrender.com/",
         githubLink: "https://github.com/HelleFH/react-listings",
         buttonText: "View Project",
         githubButtonText: "GitHub",
@@ -43,18 +43,44 @@ const projects = [
     },
     {
         id: 4,
-        name: "Cloudinary Manage",
+        name: "Cloudinary Upload/Delete",
         technologies: "MongoDB, Express, Vue, Node.js",
         images: ["/images/aiimages-screen.png"],
-        projectLink: "https://cloudinary-upload-delete-fe.onrender.com/",
+        projectLink: "https://cloudinary-upload-delete-1.onrender.com/",
         githubLink: "https://github.com/HelleFH/cloudinary-upload-delete",
         buttonText: "View Project",
         githubButtonText: "GitHub",
         descriptionHeader: "Image management with Cloudinary API",
         description: "This project is a web application for uploading and deleting images using the Cloudinary API. It is built with MongoDB, Express, Vue, and Node.js."
     },
-    {
+        {
         id: 5,
+        name: "The Monty Hall Problem",
+        technologies: "JQuery, Python, Chart.js",
+        images: ["/images/montyhall-screen.png"],
+        projectLink: "https://monty-hall-explained.onrender.com/",
+        githubLink: "https://github.com/HelleFH/product-page",
+        buttonText: "View Project",
+        githubButtonText: "GitHub",
+        descriptionHeader: "A site where users can read about and play the Monty Hall Game",
+        description: "The Monty Hall Problem is a famous counter intuitive statistics problem. 
+            This site has a simulator where users can input and amout of games to simulate using python with Flask and a manual Monty Hall Game made with JQuery.",
+    },
+            {
+        id: 6,
+        name: "A Product Portfolio",
+        technologies: "React",
+        images: ["/images/portfolio-screen.png"],
+        projectLink: "https://product-page-b2ed.onrender.com/",
+        githubLink: "https://github.com/HelleFH/Monty-Hall-Explained",
+        buttonText: "View Project",
+        githubButtonText: "GitHub",
+        descriptionHeader: "A site where users can read about and play the Monty Hall Game",
+        description: "The Monty Hall Problem is a famous counter intuitive statistics problem. 
+            This site has a simulator where users can input and amout of games to simulate using python with Flask and a manual Monty Hall Game made with JQuery.",
+    },
+    {
+        id: 7,
         name: "PHP Shopping Cart",
         technologies: "PHP, PostgreSQL, Tailwind",
         images: ["/images/phpcart-screen.png"],
@@ -66,7 +92,7 @@ const projects = [
         description: "PHP Shopping Cart is an online shopping application developed with PHP, PostgreSQL for the database, and styled with Tailwind CSS."
     },
     {
-        id: 6,
+        id: 8,
         name: "React Dating App",
         technologies: "React, JavaScript",
         images: ["/images/datingapp-screen.png"],
@@ -78,7 +104,7 @@ const projects = [
         description: "This project is a frontend for a tinder-like dating app developed using React and JavaScript. The application stores user data locally."
     },
     {
-        id: 7,
+        id: 9,
         name: "Netflix Clone",
         technologies: "JavaScript",
         images: ["/images/netflix-screen.png"],
@@ -90,11 +116,11 @@ const projects = [
         description: "Netflix Clone is a web application that replicates the user interface and functionalities of the Netflix streaming platform using JavaScript."
     },
     {
-        id: 8,
+        id: 10,
         name: "MEVN Cart",
         technologies: "MongoDB, Express, Vue, Node.js",
         images: ["/images/mevncart-screen.png"],
-        projectLink: "https://shopping-cart-master-fe.onrender.com",
+        projectLink: "https://shopping-cart-master-1-5ylu.onrender.com/",
         githubLink: "https://github.com/hellefh/shopping-cart",
         buttonText: "View Project",
         githubButtonText: "GitHub",
@@ -104,7 +130,7 @@ const projects = [
         password: "password123"
     },
     {
-        id: 9,
+        id: 11,
         name: "Lightbox Gallery",
         technologies: "JQuery",
         images: ["/images/dinosaurs-screen.png"],
@@ -116,7 +142,7 @@ const projects = [
         description: "A simple site that showcases an interactive image gallery using JQuery. Users can click on thumbnails to view full-size images in a lightbox."
     },
     {
-        id: 10,
+        id: 12,
         name: "Webshop",
         technologies: "HTML, CSS",
         images: ["/images/legekrogen-screen.png"],
